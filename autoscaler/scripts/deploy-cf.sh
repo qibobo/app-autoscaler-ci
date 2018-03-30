d45a4916202005e949840c52b3350fd9517b4106
qiyangbj@cn.ibm.com <qiyangbj@cn.ibm.com>
2018-03-30 17:27:37 +0800

    replace

--- autoscaler/scripts/deploy-cf.sh
+++ autoscaler/scripts/deploy-cf.sh
@@ -8,8 +8,38 @@ export BOSH_CLIENT=$BOSH_USERNAME
 export BOSH_CLIENT_SECRET=$BOSH_PASSWORD
 bosh -n -e vbox delete-deployment -d cf
 cd cf-deployment
-sed -i "s/enable_ssl: true/enable_ssl: false\n        port: ${CF_ROUTER_PORT}/g" cf-deployment.yml
-sed -i "s#url: https://uaa#url: http://uaa#" cf-deployment.yml
+
+cat >cf_operation.yml <<-EOF
+---
+- type: replace
+  path: /instance_groups/name=uaa/jobs/name=uaa/properties/login/protocol
+  value: http
+
+- type: replace
+  path: /instance_groups/name=uaa/jobs/name=uaa/properties/uaa/url
+  value: http://uaa.((system_domain))
+
+- type: replace
+  path: /instance_groups/name=uaa/jobs/name=uaa/properties/uaa/clients/ssh-proxy/redirect-uri
+  value: "http://uaa.((system_domain))/login"
+
+- type: replace
+  path: /instance_groups/name=api/jobs/name=cloud_controller_ng/properties/uaa/url
+  value: http://uaa.((system_domain))
+
+- type: replace
+  path: /instance_groups/name=router/jobs/name=gorouter/properties/gorouter/enable_ssl
+  value: false
+
+- type: replace
+  path: /instance_groups/name=router/jobs/name=gorouter/properties/gorouter/port?
+  value: ((cf_router_port))
+
+EOF
+
+
+# sed -i "s/enable_ssl: true/enable_ssl: false\n        port: ${CF_ROUTER_PORT}/g" cf-deployment.yml
+# sed -i "s#url: https://uaa#url: http://uaa#" cf-deployment.yml
 sed -i "s/10.244/${CF_NETWORK_PREFIX}/g" iaas-support/bosh-lite/cloud-config.yml
 sed -i "s/10.244/${CF_NETWORK_PREFIX}/g" operations/bosh-lite.yml
 bosh -n -e vbox upload-stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent
@@ -17,7 +47,9 @@ bosh -n -e vbox update-cloud-config iaas-support/bosh-lite/cloud-config.yml
 bosh -e vbox -d cf deploy -n cf-deployment.yml \
   -o operations/bosh-lite.yml \
   -o operations/use-compiled-releases.yml \
+  -o cf_operation.yml \
   --vars-store ../app-autoscaler-ci/autoscaler/deployment-vars.yml \
   -v system_domain=$CF_DOMAIN \
+  -v cf_router_port=$CF_ROUTER_PORT
   -v cf_admin_password=$CF_ADMIN_PASSWORD \
   -v uaa_admin_client_secret=$CF_ADMIN_CLIENT_SECRET
