4e80929886d655f321706e56e0b0bdf3dde33db5
qiyangbj@cn.ibm.com <qiyangbj@cn.ibm.com>
2018-04-02 11:32:29 +0800

    as operation.yml

--- autoscaler/scripts/deploy-autoscaler.sh
+++ autoscaler/scripts/deploy-autoscaler.sh
@@ -10,11 +10,23 @@ export BOSH_CLIENT_SECRET=$BOSH_PASSWORD
 cd app-autoscaler-release
 # ./scripts/update
 # sed -i -e 's/vm_type: default/vm_type: minimal/g' ./templates/app-autoscaler-deployment.yml
+
+cat >as_operation.yml <<-EOF
+---
+- type: replace
+  path: /instance_groups/name=apiserver/jobs/name=apiserver/properties/cf/api?
+  value: http://api.((system_domain)):((cf_router_port))
+- type: replace
+  path: /instance_groups/name=metricscollector/jobs/name=metricscollector/properties/cf/api?
+  value: http://api.((system_domain)):((cf_router_port))
+
+EOF
 bosh create-release --force
 bosh -n -e vbox upload-release --rebase
 
 bosh -e vbox -d app-autoscaler \
      deploy -n templates/app-autoscaler-deployment.yml \
+     -o as_operation.yml \
      --vars-store=bosh-lite/deployments/vars/autoscaler-deployment-vars.yml \
      -v system_domain=$CF_DOMAIN \
      -v cf_admin_password=$CF_ADMIN_PASSWORD \
