e79c3938d0ccbae6f0c727f9fb45699e22fee852
qibobo <qiyangbj@cn.ibm.com>
2019-10-15 10:22:15 +0800

    check

--- autoscaler/scripts/deploy-autoscaler.sh
+++ autoscaler/scripts/deploy-autoscaler.sh
@@ -21,7 +21,7 @@ cd app-autoscaler-release
 
 set +e
 autoscalerExists=$(bosh -e vbox releases | grep -c app-autoscaler)
-if [[ $autoscalerExists == 1 ]];then
+if [[ $autoscalerExists -gt 0 ]];then
     deployedCommitHash=$(bosh -e vbox releases | grep app-autoscaler | awk -F ' ' '{print $3}' | sed 's/\+//g')
     currentCommitHash=$(git log -1 --pretty=format:"%H")
     theSame=$(echo ${currentCommitHash} | grep -c ${deployedCommitHash})
